5850896617d0719339b32daf7a1d616413cbfbe4
Muneef Hameed <hey@muneef.in>
2021-04-30 20:25:43 +0530

    ui-react: add Field

--- packages/ui-react/src/index.ts
+++ packages/ui-react/src/index.ts
@@ -4,5 +4,6 @@ import { PartialTheme } from './types';
 // Components
 import Paragraph from './components/Paragraph';
 import Avatar from './components/Avatar';
+import Field from './components/Field';
 
-export { PartialTheme, Paragraph, Avatar };
+export { PartialTheme, Paragraph, Avatar, Field };
